dd95f4e9fb9b0de52ce561579672883af19904de
Ehnn09 <nelsoncanonce99@gmail.com>
2024-07-31 20:07:58 +0800

    initial commit

---
+++ src/app/message/page.jsx
@@ -0,0 +1,6 @@
+export default function Message(){
+    return (
+        <h2>Next page</h2>
+    )
+}
+
---
+++ src/app/message/pageone/pageone.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function pageone() {
+  return (
+    <div>pageone</div>
+  )
+}
