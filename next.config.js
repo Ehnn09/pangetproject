7e40ec813ad73dc9bae07a414072ae9ce3ca5cb5
Ehnn09 <nelsoncanonce99@gmail.com>
2024-11-10 17:56:55 +0800

    try deploying

--- next.config.js
+++ next.config.js
@@ -6,10 +6,4 @@ module.exports = {
     cacheMaxMemorySize: 0, // disable default in-memory caching
   }
 
-
-  module.exports = {
-    generateBuildId: async () => {
-      // This could be anything, using the latest git hash
-      return process.env.GIT_HASH
-    }
-  }
+  
